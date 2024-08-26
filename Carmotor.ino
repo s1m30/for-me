3ab3dbfc9a5863ef039ee1eb8d9cc0042084ec56
s1m30 <126210604+s1m30@users.noreply.github.com>
2024-08-27 01:47:07 +0400

    Update Carmotor.ino

--- Carmotor.ino
+++ Carmotor.ino
@@ -6,29 +6,26 @@ const int LeftMotorForward = 4;
 const int LeftMotorBackward = 5;
 const int RightMotorForward = 6;
 const int RightMotorBackward = 7;
-int speedPinLeft = 9;  // PWM pin to control speed of left motor
-int speedPinRight = 10; // PWM pin to control speed of right motor
+int speedPinLeft = 3;  // PWM pin to control speed of left motor
+int speedPinRight = 11; // PWM pin to control speed of right motor
 
-#define SONAR_NUM 3       // Number of sensors
+#define SONAR_NUM 4   // Number of sensors
 #define MAX_DISTANCE 300  // Maximum distance (in cm) to ping
 
 NewPing sonar[SONAR_NUM] = {   
-  NewPing(A1, A0, MAX_DISTANCE), // Left Trig Pin
-  NewPing(13, 12, MAX_DISTANCE),  // Center Trig Pin
-  NewPing(A3, A2, MAX_DISTANCE) // Right Trig Pin
+  NewPing(A1, A0, MAX_DISTANCE), // Left Forward Trig Pin
+  NewPing(13, 12, MAX_DISTANCE), // Center Trig Pin
+  NewPing(A3, A2, MAX_DISTANCE), // Right Forward Trig Pin
+  NewPing(A5, A4, MAX_DISTANCE) // Right Trig Pin
 };
 
 Servo Myservo;  // Servo's name
-boolean goesForward = false;
 int distance;
-
-int scanDistances[3]; // Holds the distances scanned: 0 = left, 1 = center, 2 = right
-const int scanGap = 1000; // Time gap between scans
+int scanDistances[5]; // Holds the distances scanned: 0 = left, 1 = center, 2 = right
+const int scanGap = 500; // Time gap between scans
 unsigned long scanTime = 0;
+int previousScanDistances[3]; // Holds the previous distances scanned
 
-int leftSpeed;
-int rightSpeed;
-int pwmAdjuster = 0;
 
 void setup() {
   // Set L298N Control Pins as Output
@@ -36,7 +33,7 @@ void setup() {
   pinMode(LeftMotorBackward, OUTPUT);
   pinMode(RightMotorForward, OUTPUT);
   pinMode(RightMotorBackward, OUTPUT);
-
+  
   // Setting PWM pins as outputs
   pinMode(speedPinLeft, OUTPUT);
   pinMode(speedPinRight, OUTPUT);
@@ -47,12 +44,19 @@ void setup() {
   Myservo.write(90); // Center position
   delay(2000); // Wait for 2s.
   Serial.begin(9600);
+  delay(5000); 
+   
 }
 
 void loop() {
   doScans();
-  handleObstacles();
   moveForward();
+  handleObstacles();
+  if(scanDistances[3] > 50) {
+    // Only left distance is less than 15cm
+    turnRight(delayDuration2(scanDistances[3],90,200));
+    Serial.println(scanDistances[3]);
+  }
 }
 
 void setMotorSpeed(int leftSpeed, int rightSpeed) {
@@ -68,7 +72,7 @@ void moveStop() {
 }
 
 void moveForward() {
-  setMotorSpeed(255, 255);
+  setMotorSpeed(120, 120);
   digitalWrite(RightMotorForward, HIGH);
   digitalWrite(RightMotorBackward, LOW);
   digitalWrite(LeftMotorForward, HIGH);
@@ -76,7 +80,7 @@ void moveForward() {
 }
 
 void moveBackward() {
-  setMotorSpeed(255, 255);
+  setMotorSpeed(100, 100);
   digitalWrite(RightMotorForward, LOW);
   digitalWrite(RightMotorBackward, HIGH);
   digitalWrite(LeftMotorForward, LOW);
@@ -84,14 +88,14 @@ void moveBackward() {
 }
 
 void slowDown() {
-  setMotorSpeed(100, 100);
+  setMotorSpeed(90, 90);
   digitalWrite(RightMotorForward, HIGH);
   digitalWrite(RightMotorBackward, LOW);
   digitalWrite(LeftMotorForward, HIGH);
   digitalWrite(LeftMotorBackward, LOW);
 }
 
-void turnRight(int duration) {
+void longturnRight(int duration) {
   setMotorSpeed(255, 255);
   digitalWrite(RightMotorForward, LOW);
   digitalWrite(RightMotorBackward, HIGH);
@@ -101,7 +105,7 @@ void turnRight(int duration) {
   moveStop();
 }
 
-void turnLeft(int duration) {
+void longturnLeft(int duration) {
   setMotorSpeed(255, 255);
   digitalWrite(RightMotorForward, HIGH);
   digitalWrite(RightMotorBackward, LOW);
@@ -111,47 +115,96 @@ void turnLeft(int duration) {
   moveStop();
 }
 
+void turnRight(int duration) {
+  setMotorSpeed(200, 200);
+  digitalWrite(RightMotorForward, HIGH);
+  digitalWrite(RightMotorBackward,LOW);
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(LeftMotorBackward, LOW);
+  delay(duration);
+  moveStop();
+}
+
+void turnLeft(int duration) {
+  setMotorSpeed(200, 200);
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+  digitalWrite(LeftMotorForward,HIGH );
+  digitalWrite(LeftMotorBackward, LOW);
+  delay(duration);
+  moveStop();
+}
+
+
 void doScans() {
   if (millis() > scanTime) {
     scanDistances[0] = sonar[0].ping_cm();
     scanDistances[1] = sonar[1].ping_cm();
     scanDistances[2] = sonar[2].ping_cm();
+    scanDistances[4] = sonar[3].ping_cm();
+    // Print scanned distances for debugging
+    Serial.print("Left Forward: ");
+    Serial.print(scanDistances[0]);
+    Serial.print(" cm, Center: ");
+    Serial.print(scanDistances[1]);
+    Serial.print(" cm, Right Forward: ");
+    Serial.println(scanDistances[2]);
+    
     scanTime = millis() + scanGap;
   }
 }
 
 void handleObstacles() {
-  if (scanDistances[0] <= 5 || scanDistances[1] <= 5 || scanDistances[2] <= 5) {
+  if ((scanDistances[0] <= 5 && scanDistances[1] <= 5) ||
+    (scanDistances[1] <= 5 && scanDistances[2] <= 5)||
+    (scanDistances[0] <= 5 && scanDistances[2] <= 5))  {
     moveBackward();
-    delay(1000);
+    delay(500);
     moveStop();
-    return;
+    Serial.println("Situation 0");
   }
-
+  
   if (scanDistances[0] < 15 && scanDistances[1] >= 15 && scanDistances[2] >= 15) {
     // Only left distance is less than 15cm
-    int mappedSpeed = map(scanDistances[0], 0, 20, 150, 255);
-    setMotorSpeed(mappedSpeed, 255);
-    Serial.println("yes");
-    delay(500);
-    
-  } else if (scanDistances[2] < 15 && scanDistances[0] >=15 && scanDistances[1] >= 15) {
+    turnRight(delayDuration(scanDistances[0],300,250));
+    turnLeft(150);
+    Serial.println("Situation 1");
+  } else if (scanDistances[2] < 15 && scanDistances[0] >= 15 && scanDistances[1] >= 15) {
     // Only right distance is less than 15cm
-    int mappedSpeed = map(scanDistances[2], 0, 20, 150, 255);
-    setMotorSpeed(255, mappedSpeed);
-    delay(500);
-  } else if (scanDistances[0] < 15 && scanDistances[2] < 15&& scanDistances[1] >= 15) {
+    turnLeft(delayDuration(scanDistances[2],340,250));
+    turnRight(150);
+    Serial.println("Situation 2");
+  } 
+  else if (scanDistances[0] > 15 && scanDistances[2] < 15 &&  scanDistances[1] <= 15 ){
+    // Only right distance is less than 15 cm
+    Serial.println("Obstacle detected on the left");
+    turnLeft(delayDuration(scanDistances[2],340,250));
+    turnRight(150);
+    Serial.println("Situation 3");
+    }
+    else if (scanDistances[0] < 15 && scanDistances[2] > 15 &&  scanDistances[1]<= 15 ){
+      Serial.println("Turning right");
+      // Only left distance is less than 15 cm
+      Serial.println("Obstacle detected on the right");
+      turnRight(delayDuration(scanDistances[0],330,250));
+      turnLeft(150);
+      Serial.println("Situation 4");
+} else if (scanDistances[0] < 15 && scanDistances[2] < 15 && scanDistances[1] >= 15) {
     // Both left and right distances are less than 15cm
     if (scanDistances[0] < scanDistances[2]) {
-      setMotorSpeed(150, 255); // Turn right
+      turnLeft(250);
+       // Turn right
     } else {
-      setMotorSpeed(255, 150); // Turn left
+      turnRight(250);
+       // Turn left
     }
     delay(500);
-  } else if (scanDistances[1] < 20) {
+    Serial.println("Situation 5");
+  } 
+  else if (scanDistances[1] < 15 && scanDistances[0] < 15 && scanDistances[2] < 15) {
     // Center distance is less than 20cm
     slowDown();
-    if (abs(scanDistances[0] - scanDistances[2]) <= 5) {
+    if (abs(scanDistances[0] - scanDistances[2]) <= 3) {
       // Check with servo
       Myservo.write(180); // Look left
       delay(500);
@@ -162,23 +215,30 @@ void handleObstacles() {
       int rightScan = sonar[1].ping_cm();
 
       Myservo.write(90); // Reset to center
-
+      
       if (leftScan > rightScan) {
-        turnLeft(1000);
+        longturnLeft(350);
       } else {
-        turnRight(1000);
+        longturnRight(350);
       }
+      Serial.println("Situation 6");
     } else {
       if (scanDistances[0] > scanDistances[2]) {
-        turnLeft(500);
+        turnLeft(450);
       } else {
-        turnRight(500);
+        turnRight(450);
       }
+      Serial.println("Situation 7");
     }
-  } else {
-    moveForward(); // Continue moving forward at full speed
   }
 }
 
+int delayDuration(int dir,int uplim,int lolim)
+{int dur = map(dir, 0, 15, uplim, lolim);
+ return(dur);
+}
 
-
+ int delayDuration2(int dir,int uplim,int lolim)
+{int dur = map(dir, 20, 0, uplim, lolim);
+ return(dur);
+}
